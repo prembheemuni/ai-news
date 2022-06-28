5ced7ab9d3ad9ecef418c09a822cfa0373732c59
durga bhavani <prem8309090125@gmail.com>
2022-06-28 22:22:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import "./App.css"
+
+import NewsCards from "./components/NewsCards/NewsCards"
+
+const alanKey =
+  "974840f6ac37c0b45af22cbb2fa30bff2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
+  const [newsArticles,setNewsArticles] = useState([])
+  const [activeArticle,setActiveArticle] = useState(-1)
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles,number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles)
+          setActiveArticle(-1)
+        }
+        else if(command==="highlight"){
+          setActiveArticle(prev=>prev+1)
+
+        }
+        else if(command==="open"){
+          console.log(number);
+          number = parseInt(number);
+          window.open(articles[number-1].url, "_blank")
+        }
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="heading">Voice Assistant AI Latest News Application</h1>
+      <div className="homeImage">
+      
+        <img className="img" src="https://www.pngall.com/wp-content/uploads/2018/03/Robot-PNG-Image-File.png" alt="home"/>
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
+     
     </div>
   );
 }
