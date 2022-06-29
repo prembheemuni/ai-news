9d110f35685500757394a07521d340717080cdaf
durga bhavani <prem8309090125@gmail.com>
2022-06-29 08:46:15 +0530

    open command added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
-import "./App.css"
+import "./App.css";
+import wordsToNumbers from "words-to-numbers";
 
-import NewsCards from "./components/NewsCards/NewsCards"
+import NewsCards from "./components/NewsCards/NewsCards";
 
 const alanKey =
   "974840f6ac37c0b45af22cbb2fa30bff2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
-  const [newsArticles,setNewsArticles] = useState([])
-  const [activeArticle,setActiveArticle] = useState(-1)
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles,number }) => {
+      onCommand: ({ command, articles, number }) => {
         if (command === "newHeadlines") {
-          setNewsArticles(articles)
-          setActiveArticle(-1)
-        }
-        else if(command==="highlight"){
-          setActiveArticle(prev=>prev+1)
-
-        }
-        else if(command==="open"){
-          console.log(number);
-          number = parseInt(number);
-          window.open(articles[number-1].url, "_blank")
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prev) => prev + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+          if ((parsedNumber > 20) | (parsedNumber < 0)) {
+            alanBtn().playText("Try a different number, it not exists");
+          } else if (article) {
+            window.open(article.url, "_blank");
+          }
         }
       },
     });
@@ -34,11 +39,13 @@ function App() {
     <div>
       <h1 className="heading">Voice Assistant AI Latest News Application</h1>
       <div className="homeImage">
-      
-        <img className="img" src="https://www.pngall.com/wp-content/uploads/2018/03/Robot-PNG-Image-File.png" alt="home"/>
+        <img
+          className="img"
+          src="https://www.pngall.com/wp-content/uploads/2018/03/Robot-PNG-Image-File.png"
+          alt="home"
+        />
       </div>
-      <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
-     
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 }
